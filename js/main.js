19e2a38a4407bc9eb2cebd7299ca993e4fd807c5
Simona <simonasablinaite@gmail.com>
2025-02-03 09:28:50 +0200

    Upload data to a separate file

--- js/main.js
+++ js/main.js
@@ -1,3 +1,6 @@
+import { music_list } from "./data/music-data.js";
+
+music_list
 let now_playing = document.querySelector(".now-playing");
 let track_art = document.querySelector(".track-art");
 let track_name = document.querySelector(".track-name");
@@ -9,7 +12,6 @@ let prev_btn = document.querySelector(".prev-track");
 let repeat_btn = document.querySelector(".repeat-track");
 
 let seek_slider = document.querySelector(".seek_slider");
-console.log(seek_slider);
 let volume_slider = document.querySelector(".volume_slider");
 let curr_time = document.querySelector(".current-time");
 let total_duration = document.querySelector(".total-duration");
@@ -21,69 +23,6 @@ let track_index = 0;
 let isPlaying = false;
 let isRandom = false;
 let updateTimer;
-
-const music_list = [
-   {
-      img: "assets/img/Ace-of-Base.jpg",
-      name: "All for You (Official Music Video)",
-      artist: "Ace of Base",
-      music: "assets/music/Ace of Base - All for You (Official Music Video).mp3",
-   },
-   {
-      img: "assets/img/atb.jpg",
-      name: "Ecstasy (Morten Granau Remix)",
-      artist: "ATB",
-      music: "assets/music/ATB - Ecstasy (Morten Granau Remix).mp3",
-   },
-   {
-      img: "assets/img/eiffel65.jpg",
-      name: "Voglia di dance all night",
-      artist: "Eiffel65",
-      music: "assets/music/Eiffel65 - Voglia di dance all night.mp3",
-   },
-   {
-      img: "assets/img/Eurythmics-Annie Lennox-Dave Stewart.jpg",
-      name: "Sweet Dreams (Official Video)",
-      artist: "Eurythmics, Annie Lennox, Dave Stewart",
-      music: "assets/music/Eurythmics, Annie Lennox, Dave Stewart - Sweet Dreams (Are Made Of This) (Official Video).mp3",
-   },
-   {
-      img: "assets/img/gigi-dagostino.jpg",
-      name: "Another Way",
-      artist: "Gigi D'Agostino",
-      music: "assets/music/Gigi D'Agostino Another Way Lyrics Video.mp3",
-   },
-   {
-      img: "assets/img/groove-coverage.jpg",
-      name: "7 Years & 50 Days",
-      artist: "Groove Coverage",
-      music: "assets/music/Groove Coverage - 7 Years & 50 Days (Official Video).mp3",
-   },
-   {
-      img: "assets/img/Klaas.jpg",
-      name: "Our Own Way",
-      artist: "Klaas",
-      music: "assets/music/Klaas - Our Own Way (Official Video).mp3",
-   },
-   {
-      img: "assets/img/tiesto-lay-low.jpg",
-      name: "Lay Low",
-      artist: "Tiësto",
-      music: "assets/music/Tiësto - Lay Low (Official Music Video).mp3",
-   },
-   {
-      img: "assets/img/uniting-nations.jpg",
-      name: "Out Of Touch",
-      artist: "Uniting Nations",
-      music: "assets/music/Uniting Nations - Out Of Touch (Clean Version).mp3",
-   },
-   {
-      img: "assets/img/zuchero.jpg",
-      name: "Wonderful Life",
-      artist: "Zucchero",
-      music: "assets/music/Zucchero - Wonderful Life.mp3",
-   },
-];
 
 loadTrack(track_index);
 
